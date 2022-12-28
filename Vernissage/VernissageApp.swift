392ca9a68dd0596469e68fe6349e14117421bbe2
Marcin Czachursk <mczachurski@icloud.com>
2022-12-28 09:37:16 +0100

    Initial Commit

---
+++ Vernissage/VernissageApp.swift
@@ -0,0 +1,20 @@
+//
+//  https://mczachurski.dev
+//  Copyright © 2022 Marcin Czachurski and the repository contributors.
+//  Licensed under the MIT License.
+//
+    
+
+import SwiftUI
+
+@main
+struct VernissageApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
