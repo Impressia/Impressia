94ee40ba4420e1ba6581f023118949f8648e20c0
Marcin Czachursk <mczachurski@icloud.com>
2023-02-18 15:46:32 +0100

    Pixelfed workaround.

--- MastodonKit/Sources/MastodonKit/MastodonClient+Media.swift
+++ MastodonKit/Sources/MastodonKit/MastodonClient+Media.swift
@@ -22,6 +22,33 @@ public extension MastodonClientAuthenticated {
             target: Mastodon.Media.update(id, description, focus),
             withBearerToken: token)
 
-        return try await downloadJson(UploadedAttachment.self, request: request)
+        let (data, response) = try await urlSession.data(for: request)
+        
+        // TODO: Pixelfed.social returns some list instead of single media!
+        // return try await downloadJson(UploadedAttachment.self, request: request)
+        
+        guard (response as? HTTPURLResponse)?.status?.responseType == .success else {
+            throw NetworkError.notSuccessResponse(response)
+        }
+
+        do {
+            return try JSONDecoder().decode(UploadedAttachment.self, from: data)
+        } catch {
+            do {
+                let list = try JSONDecoder().decode([UploadedAttachment].self, from: data)
+                print("[Error] Instead of single item server retured list of others media attachments!")
+                
+                if let entity = list.first(where: { item in item.id == id }) {
+                    return entity
+                }
+                
+                throw NetworkError.unknownError
+            } catch {
+                let json = String(data: data, encoding: .utf8)!
+                print(json)
+                
+                throw error
+            }
+        }
     }
 }
