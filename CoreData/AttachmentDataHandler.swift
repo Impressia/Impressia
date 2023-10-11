967d046607c885a98a413fc49c6fddcdf7585c84
Marcin Czachurski <mczachurski@icloud.com>
2023-10-11 18:42:56 +0200

    Improve widget image fetcher

--- CoreData/AttachmentDataHandler.swift
+++ CoreData/AttachmentDataHandler.swift
@@ -15,4 +15,24 @@ class AttachmentDataHandler {
         let context = viewContext ?? CoreDataHandler.shared.container.viewContext
         return AttachmentData(context: context)
     }
+    
+    func getDownloadedAttachmentData(accountId: String, length: Int, viewContext: NSManagedObjectContext? = nil) -> [AttachmentData] {
+        let context = viewContext ?? CoreDataHandler.shared.container.viewContext
+        let fetchRequest = AttachmentData.fetchRequest()
+        fetchRequest.fetchLimit = length
+
+        let sortDescriptor = NSSortDescriptor(key: "statusRelation.id", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        let predicate1 = NSPredicate(format: "statusRelation.pixelfedAccount.id = %@", accountId)
+        let predicate2 = NSPredicate(format: "data != nil")
+        fetchRequest.predicate = NSCompoundPredicate.init(type: .and, subpredicates: [predicate1, predicate2])
+
+        do {
+            return try context.fetch(fetchRequest)
+        } catch {
+            CoreDataError.shared.handle(error, message: "Error during fetching attachment data (getDownloadedAttachmentData).")
+            return []
+        }
+    }
 }
